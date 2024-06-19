6b91d0ff3303168e5585b83ecbea54218954517d
Trestan Simon <trestansimon@gmail.com>
2024-06-19 01:42:56 -0400

    Add curve point separation calculator

--- sunslicepy/tslice.py
+++ sunslicepy/tslice.py
@@ -31,23 +31,27 @@ class Slice:
         self.colormap = self.map_sequence[0].cmap
 
         cf = self.map_sequence[0].coordinate_frame
-        curve_skycoord = [SkyCoord(pair[0], pair[1], frame=cf) for pair in curve]
 
         curve_len = len(curve)
 
+        curve_skycoord = np.empty(curve_len, dtype=SkyCoord)
         curve_indices_raw = np.empty((curve_len, 2), dtype=u.Quantity)
         curve_indices = np.empty((curve_len, 2), dtype=int)
 
         for i in range(curve_len):
+            curve_skycoord[i] = SkyCoord(curve[i][0], curve[i][1], frame=cf)
             curve_indices_raw[i][0], curve_indices_raw[i][1] = self.map_sequence[0].world_to_pixel(
-                SkyCoord(curve[i][0], curve[i][1], frame=cf)
+                curve_skycoord[i]
             )
             curve_indices[i][0] = int(np.abs(np.floor(curve_indices_raw[i][0].value)))
             curve_indices[i][1] = int(np.abs(np.floor(curve_indices_raw[i][1].value)))
 
-        # curve_indices = [self.map_sequence[0].world_to_pixel(skycoord) for skycoord in curve_skycoord]
-        print(curve_indices[0][0])
+        self.arc_axis = np.empty(curve_len, dtype=u.Quantity)
+        self.arc_axis[0] = 0 * u.arcsec
+        for i in range(curve_len - 1):
+            self.arc_axis[i+1] = self.arc_axis[i] + curve_skycoord[i+1].separation(curve_skycoord[i])
 
+        print(self.arc_axis)
         self.time = np.empty(frame_count, dtype=datetime.datetime)
         self.intensity = np.zeros((frame_count, len(curve_indices)), dtype=float)
 
@@ -58,30 +62,10 @@ class Slice:
 
         self.intensity = self.intensity.T
 
-        """for frame_index in range(len(self._map_seq)):
-            self.time.append(self._map_seq[frame_index].date.datetime)
-            self._curve_coords.append(
-                SkyCoord(
-                    curve[0],
-                    curve[1],
-                    unit=(unit, unit),
-                    frame=self._map_seq[frame_index].coordinate_frame
-                )
-            )
-            self._intensity_coords.append(sunpy.map.pixelate_coord_path(
-                self._map_seq[frame_index], self._curve_coords[frame_index]
-            ))
-            self._intensity.append(sunpy.map.sample_at_coords(
-                self._map_seq[frame_index], self._intensity_coords[frame_index]
-            ))
-        print(self._curve_coords[0])
-        self._intensity = np.transpose(self._intensity)
-        self._curve = curve"""
-
     def peek(self):
         line_coords = np.arange(0, np.shape(self.intensity)[0])
         plt.pcolormesh(
-            self.time, line_coords, self.intensity,
+            self.time, [ds.value for ds in self.arc_axis], self.intensity,
             cmap=self.colormap, norm='log'
         )
         plt.show()
