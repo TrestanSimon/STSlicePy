53194df8d8cca019fe1e8b0d2111d1a401183f70
Trestan Simon <trestansimon@gmail.com>
2024-06-19 01:27:06 -0400

    Rewrite using world_to_pixel

--- sunslicepy/tslice.py
+++ sunslicepy/tslice.py
@@ -20,6 +20,7 @@ class Slice:
         self.map_sequence = seq
         self.map_sequence.all_maps_same_shape()
         frame_count = len(self.map_sequence)
+        data_slab = [smap.data for smap in seq]
 
         self.spatial_units = self.map_sequence[0].spatial_units
 
@@ -30,15 +31,30 @@ class Slice:
         self.colormap = self.map_sequence[0].cmap
 
         cf = self.map_sequence[0].coordinate_frame
-        curve_indices = [SkyCoord(pair[0], pair[1], frame=cf) for pair in curve]
+        curve_skycoord = [SkyCoord(pair[0], pair[1], frame=cf) for pair in curve]
+
+        curve_len = len(curve)
+
+        curve_indices_raw = np.empty((curve_len, 2), dtype=u.Quantity)
+        curve_indices = np.empty((curve_len, 2), dtype=int)
+
+        for i in range(curve_len):
+            curve_indices_raw[i][0], curve_indices_raw[i][1] = self.map_sequence[0].world_to_pixel(
+                SkyCoord(curve[i][0], curve[i][1], frame=cf)
+            )
+            curve_indices[i][0] = int(np.abs(np.floor(curve_indices_raw[i][0].value)))
+            curve_indices[i][1] = int(np.abs(np.floor(curve_indices_raw[i][1].value)))
+
+        # curve_indices = [self.map_sequence[0].world_to_pixel(skycoord) for skycoord in curve_skycoord]
+        print(curve_indices[0][0])
 
         self.time = np.empty(frame_count, dtype=datetime.datetime)
         self.intensity = np.zeros((frame_count, len(curve_indices)), dtype=float)
 
         for frame_index in tqdm.tqdm(range(0, frame_count)):
             self.time[frame_index] = self.map_sequence[frame_index].date.datetime
-            for i in range(0, len(curve_indices)):
-                self.intensity[frame_index][i] = sunpy.map.sample_at_coords(self.map_sequence[frame_index], curve_indices[i]).value
+            for i in range(len(curve_indices)):
+                self.intensity[frame_index][i] = data_slab[frame_index][curve_indices[i][0]][curve_indices[i][1]]
 
         self.intensity = self.intensity.T
 
