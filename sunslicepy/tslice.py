5eb2e8312775eb7de5f58e02af35d811d40b710c
Trestan Simon <trestansimon@gmail.com>
2024-06-21 12:44:11 -0400

    Break up Slice class and add running_difference

--- sunslicepy/tslice.py
+++ sunslicepy/tslice.py
@@ -1,14 +1,14 @@
-import datetime
+from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
 import numpy as np
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 from sunpy.coordinates import Helioprojective
 from sunpy.map import MapSequence
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-class Slice:
+class GenericSlice(ABC):
     def __init__(
             self,
             seq_input: MapSequence,
@@ -26,7 +26,7 @@ class Slice:
         self.time = [smap.date.datetime for smap in self.map_sequence]
         self.curve_px = np.empty((self.frame_n, self.curve_n, 2), dtype=float)
         self.curve_ds = np.empty(self.curve_n, dtype=u.Quantity)
-        self.intensity = np.zeros((self.frame_n, self.curve_n), dtype=float)
+        self.intensity = np.empty((self.frame_n, self.curve_n), dtype=float)
 
         intensity_cube = np.array([map_s.data for map_s in self.map_sequence])
 
@@ -37,38 +37,54 @@ class Slice:
         )
 
         # Necessary when points are not on disk
-        with (Helioprojective.assume_spherical_screen(
+        with Helioprojective.assume_spherical_screen(
                 center=curve_skycoords[0].observer,
                 only_off_disk=True
-        )):
-            # World to pixel by frame
-            for f in tqdm(range(self.frame_n), unit='frames'):
-                x_p, y_p = self.map_sequence[f].world_to_pixel(curve_skycoords)
-                coords = np.abs(np.array([x_p.value, y_p.value]).T)
-                for i in range(self.curve_n):
-                    self.curve_px[f][i][1], self.curve_px[f][i][0] = coords[i]
+        ):
+            self._set_slice(curve_skycoords, intensity_cube)
+            self._set_curve_ds(curve_skycoords)
 
-            # Calculate distances along curve
-            self.curve_ds[0] = 0 * u.arcsec
-            for i in range(self.curve_n-1):
-                self.curve_ds[i+1] = self.curve_ds[i] + curve_skycoords[i+1].separation(curve_skycoords[i])
+    @abstractmethod
+    def _set_slice(self, curve_skycoords, data_cube):
+        """"""
 
-        self._line_draw(intensity_cube)
-        self.intensity = self.intensity.T
+    @abstractmethod
+    def _set_curve_ds(self, curve_skycoords):
+        """"""
 
-    def _line_draw(self, data_cube):
+    def peek(self, norm='log'):
+        plt.pcolormesh(
+            self.time, [ds.value for ds in self.curve_ds], self.intensity.T,
+            cmap=self.colormap, norm=norm
+        )
+        plt.show()
+
+    @property
+    def running_difference(self):
+        difference = np.empty((self.frame_n-1, self.curve_n), dtype=float)
+        for f in range(self.frame_n-1):
+            for i in range(self.curve_n):
+                difference[f][i] = self.intensity[f+1][i] - self.intensity[f][i]
+        return difference
+
+
+class NaiveSlice(GenericSlice):
+    def _set_slice(self, curve_skycoords, data_cube):
+        # World to pixel by frame
+        for f in tqdm(range(self.frame_n), unit='frames'):
+            x_p, y_p = self.map_sequence[f].world_to_pixel(curve_skycoords)
+            coords = np.abs(np.array([x_p.value, y_p.value]).T)
+            for i in range(self.curve_n):
+                self.curve_px[f][i][1], self.curve_px[f][i][0] = coords[i]
+
         for f in range(self.frame_n):
             for i in range(self.curve_n):
                 x_i = int(np.floor(self.curve_px[f][i][0]))
                 y_i = int(np.floor(self.curve_px[f][i][1]))
                 self.intensity[f][i] = data_cube[f][x_i][y_i]
 
-    def peek(self, norm='log'):
-        plt.pcolormesh(
-            self.time, [ds.value for ds in self.curve_ds], self.intensity,
-            cmap=self.colormap, norm=norm
-        )
-        plt.show()
-
-    def running_difference(self):
-        pass
+    def _set_curve_ds(self, curve_skycoords):
+        # Calculate distances along curve
+        self.curve_ds[0] = 0 * u.arcsec
+        for i in range(self.curve_n - 1):
+            self.curve_ds[i + 1] = self.curve_ds[i] + curve_skycoords[i + 1].separation(curve_skycoords[i])
