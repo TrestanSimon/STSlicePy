c1266594f46a503e5779c76ebab02bf9907ec56a
Trestan Simon <trestansimon@gmail.com>
2024-07-04 14:17:35 -0400

    Use correlation

--- sunslicepy/processing.py
+++ sunslicepy/processing.py
@@ -3,9 +3,9 @@ import scipy
 
 
 def running_difference(time_space_arr: np.ndarray) -> np.ndarray:
-    return scipy.ndimage.convolve(
+    return scipy.ndimage.correlate(
         time_space_arr,
-        np.flip(np.array([[-1, 1, 0]]).T)
+        np.array([[-1, 1, 0]]).T,
     )
 
 
@@ -17,31 +17,29 @@ def base_difference(time_space_arr: np.ndarray):
     return difference
 
 
-def boxcar_mask(
-        time_space_arr: np.ndarray,
-        t: int,
-        x: int,
-        **kwargs,
-):
-    return scipy.ndimage.convolve(
+def boxcar_mask(time_space_arr: np.ndarray, t: int, x: int, **kwargs):
+    return scipy.ndimage.correlate(
         time_space_arr,
-        np.ones((t, x)) / float(t * x)
+        np.ones((t, x)) / float(t * x),
+        **kwargs
     )
 
 
-def gradient_mask(time_space_arr: np.ndarray):
-    return scipy.ndimage.convolve(
+def gradient_mask(time_space_arr: np.ndarray, **kwargs):
+    return scipy.ndimage.correlate(
         time_space_arr,
-        np.flip(np.array([[-1, 0, 1]]).T)
+        np.array([[-1, 0, 1]]).T,
+        **kwargs
     )
 
 
-def sobel_mask(time_space_arr: np.ndarray):
-    return scipy.ndimage.convolve(
+def sobel_mask(time_space_arr: np.ndarray, **kwargs):
+    return scipy.ndimage.correlate(
         time_space_arr,
-        np.flip(np.array([
+        np.array([
             [-1, 0, 1],
             [-2, 0, 2],
             [-1, 0, 1],
-        ]).T)
+        ]).T,
+        **kwargs
     )
